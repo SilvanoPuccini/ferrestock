47dd01d7ae196a5f0534cab3dd9e9f8ab91045dc
Grupo2 <grupo2@test.com>
2026-03-13 13:59:15 -0300

    feat: agregar Cloudinary para archivos de media

--- config/settings/base.py
+++ config/settings/base.py
@@ -96,3 +96,11 @@ LOGIN_REDIRECT_URL = "dashboard:home"
 LOGOUT_REDIRECT_URL = "login"
 LOGIN_URL = "login"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+}
